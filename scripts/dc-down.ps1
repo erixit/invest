38053bbc987fb9fa568ece56b7eb516b2ae3d717
Erik Vande Velde <erikv@telenet.be>
2026-02-11 21:28:09 +0100

    make invest work on windows/msi

---
+++ scripts/dc-down.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = "Stop"
+
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$projectDir = Split-Path -Parent $scriptDir
+Set-Location $projectDir
+
+# Default to the docker profile unless caller already set COMPOSE_PROFILES.
+if (-not $env:COMPOSE_PROFILES) {
+    $env:COMPOSE_PROFILES = "docker"
+}
+
+$services = docker compose config --services | Where-Object { $_ -notmatch "_debug$" }
+if ($services) {
+    docker compose stop $services
+    docker compose rm -f $services
+}
